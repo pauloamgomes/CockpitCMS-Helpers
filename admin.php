de033d4b419f6d154165c0a3bce6d34cc09f721d
Paulo Gomes <pauloamgomes@gmail.com>
2018-12-03 12:00:38 +0000

    added assets menu permission

--- admin.php
+++ admin.php
@@ -8,33 +8,34 @@
 // Module ACL definitions.
 $this("acl")->addResource('helpers', [
   'jsonview',
-  'quickcreate'
+  'quickcreate',
+  'assets'
 ]);
 
 
 $this->on('admin.init', function () use ($app) {
 
-  $settings = $app->config['helpers'] ?? [];
   $this->helper('admin')->addAssets('helpers:assets/css/helpers.css');
+  $settings = $app->config['helpers'] ?? [];
+
+  // Add assets to modules menu.
+  if ($app->module('cockpit')->hasaccess('helpers', 'assets')) {
+    $this('admin')->addMenuItem('modules', [
+      'label' => 'Assets',
+      'icon' => 'assets:app/media/icons/assets.svg',
+      'route' => '/assetsmanager',
+      'active' => strpos($this['route'], '/assetsmanager') === 0,
+    ]);
+  }
 
   if (!empty($settings['environment']) && $app->path('helpers:assets/css/helpers-' . $settings['environment'] . '.css')) {
     $this->helper('admin')->addAssets('helpers:assets/css/helpers-' . $settings['environment'] . '.css');
   }
 
-  if ($app->module('cockpit')->hasaccess('helpers', 'quickactions')) {
-    // Check we have quickactions in the configuration.
-    $config = $app->config['helpers'];
-    if (!empty($config['quickactions'])) {
-      $this->helper('admin')->addAssets('helpers:assets/cp-quickactions.tag');
-      $this->helper('admin')->addAssets('helpers:assets/quickactions.js');
-      // Add assets to modules menu.
-      $this('admin')->addMenuItem('modules', [
-        'label' => 'Assets',
-        'icon' => 'assets:app/media/icons/assets.svg',
-        'route' => '/assetsmanager',
-        'active' => strpos($this['route'], '/assetsmanager') === 0,
-      ]);
-    }
+  // Check we have quickactions in the configuration.
+  if (!empty($settings['quickactions']) && $app->module('cockpit')->hasaccess('helpers', 'quickactions')) {
+    $this->helper('admin')->addAssets('helpers:assets/cp-quickactions.tag');
+    $this->helper('admin')->addAssets('helpers:assets/quickactions.js');
   }
 });
 
