e517e202cdd2c6292482bc845726f5ae9592f92c
Paulo Gomes <pauloamgomes@gmail.com>
2019-02-04 14:44:12 +0000

    increased timeout to avoid riot errors during page load

--- assets/quickactions.js
+++ assets/quickactions.js
@@ -3,4 +3,4 @@ var $this = this;
 window.setTimeout(function() {
   App.$('.app-modulesbar').append(App.$('<li><cp-quickactions /></li>').addClass('quickactions'));
   riot.mount('cp-quickactions', {});
-}, 50);
+}, 500);
